c0f15d31e0a6f43fba3e1332c96e3f123ec64ddc
isrzanza <46667487+isrzanza@users.noreply.github.com>
2021-01-25 13:55:56 +0200

    cleanup

--- app/src/main/java/com/dorcohen/scavjunkbox/data/database/Database.kt
+++ app/src/main/java/com/dorcohen/scavjunkbox/data/database/Database.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.dorcohen.scavjunkbox.data.model.AppInfo
 
-@Database(entities = [AppInfo::class], version = 1)
+@Database(entities = [AppInfo::class], version = 1, exportSchema = false)
 abstract class Database : RoomDatabase(){
     abstract fun mainDao(): MainDao
 }
